9d3f1420d0c69bc0a0e0f2b965ff7f245781ae49
yasmeen <nnanahamoud123@gmail.com>
2025-05-19 23:39:28 +0300

    Add files via upload

---
+++ js/chat.js
@@ -0,0 +1,11 @@
+var Tawk_API = Tawk_API || {},
+Tawk_LoadStart = new Date();
+(function () {
+var s1 = document.createElement("script"),
+  s0 = document.getElementsByTagName("script")[0];
+s1.async = true;
+s1.src = "https://embed.tawk.to/682b5658714ec01918f30eab/1irkkiipi";
+s1.charset = "UTF-8";
+s1.setAttribute("crossorigin", "*");
+s0.parentNode.insertBefore(s1, s0);
+})();
